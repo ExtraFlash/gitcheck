68868b84f774e79ae8c3a500e1bbf0a60660affc
ExtraFlash <orshkuri2000@gmail.com>
2019-12-06 15:38:47 +0200

    initial

---
+++ app/src/main/java/com/os_games/checkgit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.os_games.checkgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
